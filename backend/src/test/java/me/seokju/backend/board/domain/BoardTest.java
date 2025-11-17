f2d3cd0c255c578ef07cadba507a65cd8ef1d1a3
somefood <iamseokjuhong@gmail.com>
2025-11-17 22:56:29 +0900

    @DisplayName 추가

--- backend/src/test/java/me/seokju/backend/board/domain/BoardTest.java
+++ backend/src/test/java/me/seokju/backend/board/domain/BoardTest.java
@@ -1,5 +1,6 @@
 package me.seokju.backend.board.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,20 +8,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BoardTest {
 
     @Test
+    @DisplayName("게시글을 생성한다")
     void create() {
         var board = Board.create("title", "content");
 
         assertThat(board.getTitle()).isEqualTo("title");
         assertThat(board.getContent()).isEqualTo("content");
         assertThat(board.getStatus()).isEqualTo(BoardStatus.ACTIVE);
+        assertThat(board.getCreatedAt()).isNotNull();
     }
 
     @Test
+    @DisplayName("게시글을 삭제한다")
     void delete() {
         var board = Board.create("title", "content");
 
         board.delete();
 
         assertThat(board.getStatus()).isEqualTo(BoardStatus.DELETED);
+        assertThat(board.getUpdatedAt()).isNotNull();
     }
 }
