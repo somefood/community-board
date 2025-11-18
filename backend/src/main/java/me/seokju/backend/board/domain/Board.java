fee8ae6d38f7683e16538cf60abd18b7cca27d50
Seokju Hong <iamseokjuhong@gmail.com>
2025-11-18 13:05:36 +0900

    feat: 게시판 기능 구현
    - Board 엔티티 및 리포지토리 추가
    - BoardService 클래스 생성 및 게시판 생성/삭제 기능 구현
    - application.properties 파일에 JPA SQL 출력 설정 추가
    - BoardService 테스트 케이스 작성

--- backend/src/main/java/me/seokju/backend/board/domain/Board.java
+++ backend/src/main/java/me/seokju/backend/board/domain/Board.java
@@ -1,12 +1,22 @@
 package me.seokju.backend.board.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class Board {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
